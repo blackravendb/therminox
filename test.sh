34751a676354cbbff40f80239e1762e75f178bb6
blackravendb <dennis.brandmueller@stud.fh-rosenheim.descript type=text/javascript  /* ![CDATA[ */ (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2013-11-25 19:44:49 +0100

    test.sh gelöscht

--- test.sh
+++
@@ -1,5 +0,0 @@
-git config --global user.email "dennis.brandmueller@stud.fh-rosenheim.de<script type="text/javascript">  
-/* <![CDATA[ */ 
-(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName("script");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); 
-/* ]]> */  
-</script>" 
