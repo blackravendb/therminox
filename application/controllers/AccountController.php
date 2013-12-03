01a4997fb5c1a3b8a36d0adc0217528231eb9187
www-data <www-data@debian.fritz.box>
2013-12-03 18:57:16 +0100

    additional Controller: - AccountController
    included Actions:
    - loginAction
    - logoutAction
    - registerAction
    - confirmAction
    - recoverAction
    - passwordAction
    - profileAction
    
    additional Forms:
    - Login
    - Register

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,67 @@
+<?php
+
+class AccountController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+    public function logoutAction()
+    {
+        // action body
+    }
+
+    public function registerAction()
+    {
+        // action body
+    }
+
+    public function confirmAction()
+    {
+        // action body
+    }
+
+    public function recoverAction()
+    {
+        // action body
+    }
+
+    public function passwordAction()
+    {
+        // action body
+    }
+
+    public function profileAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
