79c16c52db4a4795712ad4acf3715cd5f1165fe7
Lukas Schmitt <lukas.a.schmitt@gmail.com>
2013-12-05 22:35:06 +0100

    Updated and improved forms

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -3,6 +3,21 @@
 class Application_Form_Login extends Zend_Form
 {
 
+	public $elementDecorators = array(
+			'ViewHelper',
+			'Errors',
+			array(array('data' => 'HtmlTag'), array('tag' => 'td', 'class' => 'element')),
+			array('Label', array('tag' => 'td')),
+			array(array('row' => 'HtmlTag'), array('tag' => 'tr')),
+	);
+	
+	public $buttonDecorators = array(
+			'ViewHelper',
+			array(array('data' => 'HtmlTag'), array('tag' => 'td', 'class' => 'element')),
+			array(array('label' => 'HtmlTag'), array('tag' => 'td', 'placement' => 'prepend')),
+			array(array('row' => 'HtmlTag'), array('tag' => 'tr')),
+	);
+	
     public function init()
     {
     	$this->setMethod('post')
@@ -13,23 +28,30 @@ class Application_Form_Login extends Zend_Form
     	$email = new Zend_Form_Element_Text('email');
     	$email->setRequired(true)
     		  ->setLabel('Benutzername')
-    		  ->setDecorators(array('ViewHelper', 'Label', 'Errors'))
+    		  ->setDecorators($this->elementDecorators)
     		  ->addErrorMessage('Bitte geben Sie Ihren Benutzernamen an.');
  
     	$password = new Zend_Form_Element_Password('password');
     	$password->setRequired(true)
     			 ->setLabel('Passwort')
-    			 ->setDecorators(array('ViewHelper', 'Label', 'Errors'))
+    			 ->setDecorators($this->elementDecorators)
     			 ->addErrorMessage('Bitte geben Sie Ihr Passwort an.');
     	
     	$submit = new Zend_Form_Element_Submit('submit');
     	$submit->setLabel('Anmelden')
-    		   ->setDecorators(array('ViewHelper'));
+    		   ->setDecorators($this->buttonDecorators);
     		     	
-    	$this->setDecorators( array( array('ViewScript', array('viewScript' => '_form_login.phtml'))));
     	$this->addElements(array($email, $password, $submit));
     }
 
+    public function loadDefaultDecorators()
+    {
+    	$this->setDecorators(array(
+    			'FormElements',
+    			array('HtmlTag', array('tag' => 'table')),
+    			'Form',
+    	));
+    }
 
 }
 
--- application/forms/Register.php
+++ application/forms/Register.php
@@ -3,6 +3,21 @@
 class Application_Form_Register extends Zend_Form
 {
 
+	public $elementDecorators = array(
+			'ViewHelper',
+			'Errors',
+			array(array('data' => 'HtmlTag'), array('tag' => 'td', 'class' => 'element')),
+			array('Label', array('tag' => 'td')),
+			array(array('row' => 'HtmlTag'), array('tag' => 'tr')),
+	);
+	
+	public $buttonDecorators = array(
+			'ViewHelper',
+			array(array('data' => 'HtmlTag'), array('tag' => 'td', 'class' => 'element')),
+			array(array('label' => 'HtmlTag'), array('tag' => 'td', 'placement' => 'prepend')),
+			array(array('row' => 'HtmlTag'), array('tag' => 'tr')),
+	);
+	
     public function init()
     {
     	$this->setMethod('post')
@@ -12,28 +27,29 @@ class Application_Form_Register extends Zend_Form
     	$email = new Zend_Form_Element_Text('email');
     	$email->setRequired(true)
     	->setLabel('Email-Adresse')
-    	->setDecorators(array('ViewHelper', 'Label', 'Errors'))
-    	->addErrorMessage('Bitte geben Sie eine gültige Email-Adresse an.')
-    	->addValidator('emailAddress');
+    	->setDecorators($this->elementDecorators)
+    	->setValidators(array(
+    		array('NotEmpty', true),
+            array('EmailAddress', true)
+        ))
+    	->addErrorMessage('Bitte geben Sie eine gültige Email-Adresse an.');
     	 
     	$password = new Zend_Form_Element_Password('password');
     	$password->setRequired(true)
     	->setLabel('Passwort')
-    	->setDecorators(array('ViewHelper', 'Label', 'Errors'))
+    	->setDecorators($this->elementDecorators)
     	->addErrorMessage('Bitte geben Sie ein Passwort an.');
     	
-
     	$confirm_password = new Zend_Form_Element_Password('confirm_password');
     	$confirm_password->setRequired(true)
     	->setLabel('Passwort bestätigen')
-    	->setDecorators(array('ViewHelper', 'Label', 'Errors'))
+    	->setDecorators($this->elementDecorators)
     	->addErrorMessage('Die Passwörter stimmen nicht überein.')
     	->addValidator('Identical', false, array('token' => 'password'));
     	
     	$title = new Zend_Form_Element_Select('title');
     	$title->setLabel('Anrede')
-    	->setDecorators(array('ViewHelper', 'Label', 'Errors'))
-    	->addErrorMessage('Bitte geben Sie Ihren Vornamen an.')
+    	->setDecorators($this->elementDecorators)
     	->addMultiOptions(array(
     			'herr' => 'Herr',
     			'frau' => 'Frau'
@@ -43,25 +59,43 @@ class Application_Form_Register extends Zend_Form
     	$name = new Zend_Form_Element_Text('name');
     	$name->setRequired(true)
     	->setLabel('Vorname')
-    	->setDecorators(array('ViewHelper', 'Label', 'Errors'))
-    	->addErrorMessage('Bitte geben Sie Ihren Vornamen an.')
-    	->addValidator('Alpha');
+    	->setDecorators($this->elementDecorators)
+    	->setValidators(array(
+    		array('NotEmpty', true),
+            array('Alpha', true, array('allowWhiteSpace' => true)),
+            array('StringLength', true, array('min' => 2, 'max' => 50))
+        ));
+    	$name->getValidator('NotEmpty')->setMessage('Bitte geben Sie Ihren Vornamen an.');
+    	$name->getValidator('Alpha')->setMessage('Das Feld darf nur Buchstaben enthalten.');
+    	$name->getValidator('StringLength')->setMessage('Der Vorname muss zwischen 2 und 50 Zeichen lang sein.');
     	
     	$lastname = new Zend_Form_Element_Text('lastname');
     	$lastname->setRequired(true)
     	->setLabel('Nachname')
-    	->setDecorators(array('ViewHelper', 'Label', 'Errors'))
-    	->addErrorMessage('Bitte geben Sie Ihren Nachnamen an.')
-    	->addValidator('Alpha');
+    	->setDecorators($this->elementDecorators)
+    	->setValidators(array(
+    		array('NotEmpty', true),
+            array('Alpha', true, array('allowWhiteSpace' => true)),
+            array('StringLength', true, array('min' => 2, 'max' => 50))
+        ));
+    	$lastname->getValidator('NotEmpty')->setMessage('Bitte geben Sie Ihren Nachnamen an.');
+        $lastname->getValidator('Alpha')->setMessage('Das Feld darf nur Buchstaben enthalten.');
+    	$lastname->getValidator('StringLength')->setMessage('Der Nachname muss zwischen 2 und 50 Zeichen lang sein.');
     	 
     	$submit = new Zend_Form_Element_Submit('submit');
     	$submit->setLabel('Registrieren')
-    	->setDecorators(array('ViewHelper'));
+    	->setDecorators($this->buttonDecorators);
     	
-    	$this->setDecorators( array( array('ViewScript', array('viewScript' => '_form_register.phtml'))));
     	$this->addElements(array($email, $password, $confirm_password, $title, $name, $lastname, $submit));
     }
 
-
+    public function loadDefaultDecorators()
+    {
+    	$this->setDecorators(array(
+    			'FormElements',
+    			array('HtmlTag', array('tag' => 'table')),
+    			'Form',
+    	));
+    }
 }
 
