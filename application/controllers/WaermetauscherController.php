e137fa1c6ba12d4cf95825c6f2ac48eddb86dd4e
Olli92 <oliver-alraun@hotmail.de>
2013-12-12 15:54:06 +0100

    Produktberater

--- application/controllers/WaermetauscherController.php
+++ application/controllers/WaermetauscherController.php
@@ -5,12 +5,37 @@ class WaermetauscherController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->view->title = "Produktberater";
+        
+        $form = new Application_Form_ProduktberaterWt();
+        $this->view->produktberater = $form;
+        
+        if($produktberater->_request->isPost())
+        {
+        	$formData = $this->_request->getPost();
+        	
+        	if($produktberater->isValid($formData))
+        	{
+        		$temp = $produktberater->getValue('Temperatur');
+        		$einsatzgbt = $produktberater->getValue('Einsatzgebiet');
+        		$anschluss = $produktberater->getValue('Anschluss');
+        		$maxHeight = $produktberater->getValue('Hoehe');
+        		$maxWidth = $produktberater->getValue('Breite');
+        		
+        		if($einsatzgbt != null){
+        			//TODO
+        			//Datenbankabfragen
+        		}
+        	}
+        	else{
+        		$produktberater->populate($formData);
+        	}
+        }
     }
 
     public function indexAction()
     {
-        // action body
+        
     }
 
     public function geloetetAction()
