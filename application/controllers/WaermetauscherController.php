dc059981f47ffc5c9bc3425c3753c5094b87de52
www-data <www-data@debian.fritz.box>
2013-12-12 13:51:44 +0100

    Waermetauscher Controller erzeugt; geloetet, geschraubt, rohrbuendel action in Waermetauscher erzeugt 	modified:   .zfproject.xml 	new file:   application/controllers/WaermetauscherController.php 	new file:   application/views/scripts/waermetauscher/geloetet.phtml 	new file:   application/views/scripts/waermetauscher/geschraubt.phtml 	new file:   application/views/scripts/waermetauscher/index.phtml 	new file:   application/views/scripts/waermetauscher/rohrbuendel.phtml

---
+++ application/controllers/WaermetauscherController.php
@@ -0,0 +1,39 @@
+<?php
+
+class WaermetauscherController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function geloetetAction()
+    {
+        // action body
+    }
+
+    public function geschraubtAction()
+    {
+        // action body
+    }
+
+    public function rohrbuendelAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
