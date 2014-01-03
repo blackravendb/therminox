e8b5fe3fdb85244330df1415056ce1b9f886965c
blackravendb <dennis.brandmueller@stud.fh-rosenheim.de>
2014-01-03 13:41:20 +0100

    DB Table hinzugefügt WaermetauscherGeletet, Stutzenmaterial; weitere Änderungen an BenutzerMapper + Tabllen 	modified:   .zfproject.xml 	modified:   application/models/DbTable/WaermetauscherGeloetet.php 	new file:   application/models/Stutzenmaterial.php 	new file:   application/models/WaermetauscherGeloetet.php 	modified:   application/views/scripts/index/index.phtml 	new file:   library/AbstractClass/ModelTableAbstract.php 	new file:   library/Interface/ITable.php

--- application/models/DbTable/WaermetauscherGeloetet.php
+++ application/models/DbTable/WaermetauscherGeloetet.php
@@ -4,7 +4,26 @@ class Application_Model_DbTable_WaermetauscherGeloetet extends Zend_Db_Table_Abs
 {
 
     protected $_name = 'waermetauscherGeloetet';
-
+    protected $_primary = 'id';
+    
+    protected $_dependentTable = array('Stutzenmaterial');
+    
+    protected $_referenceMap    = array(
+    		'Benutzer_Lieferadresse' => array(
+    				'columns'           => 'id',
+    				'refTableClass'     => 'Application_Model_DbTable_blub',
+    				'refColumns'        => 'blub_blub',
+    				'onDelete'			=> 'self::RESTRICT',
+    				'onUpdate'			=> 'self::RESTRICT'
+    		)
+    		);
+    
+    protected $select;
+    
+    public function init() {
+    	$this->select = $this->select()
+    	->setIntegrityCheck(false);
+    }
 
 }
 
---
+++ library/Interface/ITable.php
@@ -0,0 +1,4 @@
+<?php
+interface ITable{
+	public function update(Library_AbstractClass_ModelTableAbstract $object);
+}
