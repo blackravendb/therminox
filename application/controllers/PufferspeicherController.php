9260938aeb3e4a292c4f14fd6ea6dba47330004d
SimoKutlin <simo.kutlin@gmail.com>
2014-01-07 17:57:11 +0100

    Pufferspeicher teils online, Wärmetauscher update
    Pufferspeicher VVX fertig, LAS folgt sobald DB bereit
    Angebotscontroller, Wassererwärmer wartet noch auf DB

--- application/controllers/PufferspeicherController.php
+++ application/controllers/PufferspeicherController.php
@@ -109,33 +109,11 @@ class PufferspeicherController extends Zend_Controller_Action
 		$request = $this->getRequest ();
 		$art = $request->getParam ( 'artikel' );
 		if (null != $art) {
-			// Artikeldaten aus db auslesen und aufbereiten
-			// Statement: SELECT * FROM Pufferspeicher WHERE Kategorie = 'VVX' AND ID like ?, $art;
-			$entries = array ();
-			$art_name = "Testartikel"; // $entries['Artikelname'];
-			$art_dim = 503; // $entries['Fassungsvermögen'];
-			$art_conn_zirk = 40; // $entries['Anschluss_Zirkulation'];
-			$art_conn_cold = 30; // $entries['Anschluss_Kaltwasser'];
-			$art_conn_warm = 30; // $entries['Anschluss_Warmwasser'];
-			$art_conn_load = 30; // $entries['Anschluss_Ladestutzen'];
-			$art_conn_sens = 30; // $entries['Anschluss_Thermometer'];
-			$art_height = 1230; // $entries['Maximale Höhe'];
-			$art_width = 123; // $entries['Maximale Breite'];
+			$db_mapper = new Application_Model_PufferspeicherMapper();
+			$data_object = $db_mapper->getPufferspeicherByModel($art);
 			
-			$container = array (
-					$art,
-					$art_name,
-					$art_dim,
-					$art_height,
-					$art_width,
-					$art_conn_warm,
-					$art_conn_cold,
-					$art_conn_zirk,
-					$art_conn_load,
-					$art_conn_sens 
-			);
 			
-			$this->view->art_display = $container;
+			$this->view->dbdata = $data_object;
 		} else {
 			$this->view->message = 'Artikel konnte nicht gefunden werden';
 			if (isset ( $_SERVER ['HTTP_REFERER'] )) {
--- application/controllers/WaermetauscherController.php
+++ application/controllers/WaermetauscherController.php
@@ -1,28 +1,15 @@
  <?php
-class WaermetauscherController extends Zend_Controller_Action
-{
-
-    public function init()
-    {	//session_start();
-    	require_once 'Cart/ShoppingCart.php';
-    	$this->view->showVor = false;
-    	$this->view->keineVorschläge = false;
-    }
-
-    public function indexAction()
-    {
-
-     $this->view->title = "ProduktberaterWärmetauscher";
-    	/*
-     	$minTemp = 155;
-     	$maxTemp = 195;
-     	$minHeight = 170;
-     	$maxHeight = 1100;
-     	$minWidth = 70;
-     	$maxWidth = 400;
-     	$minLength = 20;
-     	$maxLength = 500;
-     	*/
+	class WaermetauscherController extends Zend_Controller_Action {
+		public function init() { // session_start();
+			require_once 'Cart/ShoppingCart.php';
+			$this->view->showVor = false;
+			$this->view->keineVorschläge = false;
+		}
+		public function indexAction() {
+			$this->view->title = "ProduktberaterWärmetauscher";
+			/*
+			 * $minTemp = 155; $maxTemp = 195; $minHeight = 170; $maxHeight = 1100; $minWidth = 70; $maxWidth = 400; $minLength = 20; $maxLength = 500;
+			 */
      
      	/*
         $form = new Application_Form_ProduktberaterWt(array('minTemp' => '155', 'maxTemp' => '195', 
@@ -31,80 +18,74 @@ class WaermetauscherController extends Zend_Controller_Action
         												'minLength' => '20', 'maxLength' => '500'));
        */
      	
-     	$form = new Application_Form_ProduktberaterWt();
-     	
-        $form->setMethod('post');
-        
-        $this->view->produktberaterWt = $form;
-        
-        if($this->_request->isPost())
-        {
-        	$formData = $this->_request->getPost();
-        	
-        	if($form->isValid($formData))
-        	{
-        		$minTemp = $form->getValue('TemperaturMin');
-        		$maxTemp = $form->getValue('TemperaturMax');
-        		$einsatzgbt = $form->getValue('Einsatzgebiet');
-        		$anschluss = $form->getValue('Anschluss'); //Nur angehackte Werte werden übergeben
-        		$minHeight = $form->getValue('HoeheMin');
-        		$maxHeight = $form->getValue('HoeheMax');
-        		$minWidth = $form->getValue('BreiteMin');
-        		$maxWidth = $form->getValue('BreiteMax');
-        		
-        		$anzahlAnschlüsse = 0;
-        		
-        		$wtmapper = new Application_Model_WaermetauscherMapper();
-        		
-        		if(!empty($minTemp)){ 
-        			$wtmapper->setTemperaturMin($minTemp);
-        			}
-        		
-        		if(!empty($maxTemp)){ 
-        			$wtmapper->setTemperaturMax($maxTemp);
-        			}
-        		
-        		if(!(strcmp ($einsatzgbt, 'Bitte wählen') == 0)){ //funktioniert
-        			$wtmapper->setEinsatzgebiet($einsatzgbt); //wenn standartwert "bitte wählen" dasteht nicht set!
-        		}
-        		
-        		if(count($anschluss) != 3){ //TODO 
-        			$wtmapper->setAnschluss($anschluss); //if (kein set, wenn alle angehackt)
-        		}
-        		
-        		if(!empty($minHeight)){
-        			$wtmapper->setHoeheMin($minHeight);
-        		}
-        		
-        		if(!empty($maxHeight)){
-        			$wtmapper->setHoeheMax($maxHeight);
-        		}
-        		
-        		if(!empty($minWidth)){
-        			$wtmapper->setBreiteMin($minWidth);
-        		}
-        		
-        		if(!empty($maxWidth)){
-        			$wtmapper->setBreiteMax($maxWidth);
-        		}
-        		
-        		
-        		$produkte = $wtmapper->getWaermetauscher(); //funktioniert, ist immer ein Array
-        		
-        		if(!empty($produkte)){ //Sobald nicht alle 3 Anschlüsse ausgewählt sind, ist das Array leer
-        			$this->view->vorschläge = $produkte;
-        		}else{
-        			$this->view->keineVorschläge = true;
-        		}
-        		
-        		$this->view->showVor = true; //Vorschläge werden angezeigt
-        		
-        		/*
-        		$tempVal = $form->getElement('tempVal');
-        		$heightVal = $form->getElement('heightVal');
-        		$widthVal = $form->getElement('widthVal');
-        		$lengthVal = $form->getElement('lengthVal');
-        		*/
+     	$form = new Application_Form_ProduktberaterWt ();
+			
+			$form->setMethod ( 'post' );
+			
+			$this->view->produktberaterWt = $form;
+			
+			if ($this->_request->isPost ()) {
+				$formData = $this->_request->getPost ();
+				
+				if ($form->isValid ( $formData )) {
+					$minTemp = $form->getValue ( 'TemperaturMin' );
+					$maxTemp = $form->getValue ( 'TemperaturMax' );
+					$einsatzgbt = $form->getValue ( 'Einsatzgebiet' );
+					$anschluss = $form->getValue ( 'Anschluss' ); // Nur angehackte Werte werden übergeben
+					$minHeight = $form->getValue ( 'HoeheMin' );
+					$maxHeight = $form->getValue ( 'HoeheMax' );
+					$minWidth = $form->getValue ( 'BreiteMin' );
+					$maxWidth = $form->getValue ( 'BreiteMax' );
+					
+					$anzahlAnschlüsse = 0;
+					
+					$wtmapper = new Application_Model_WaermetauscherMapper ();
+					
+					if (! empty ( $minTemp )) {
+						$wtmapper->setTemperaturMin ( $minTemp );
+					}
+					
+					if (! empty ( $maxTemp )) {
+						$wtmapper->setTemperaturMax ( $maxTemp );
+					}
+					
+					if (! (strcmp ( $einsatzgbt, 'Bitte wählen' ) == 0)) { // funktioniert
+						$wtmapper->setEinsatzgebiet ( $einsatzgbt ); // wenn standartwert "bitte wählen" dasteht nicht set!
+					}
+					
+					if (count ( $anschluss ) != 3) { // TODO
+						$wtmapper->setAnschluss ( $anschluss ); // if (kein set, wenn alle angehackt)
+					}
+					
+					if (! empty ( $minHeight )) {
+						$wtmapper->setHoeheMin ( $minHeight );
+					}
+					
+					if (! empty ( $maxHeight )) {
+						$wtmapper->setHoeheMax ( $maxHeight );
+					}
+					
+					if (! empty ( $minWidth )) {
+						$wtmapper->setBreiteMin ( $minWidth );
+					}
+					
+					if (! empty ( $maxWidth )) {
+						$wtmapper->setBreiteMax ( $maxWidth );
+					}
+					
+					$produkte = $wtmapper->getWaermetauscher (); // funktioniert, ist immer ein Array
+					
+					if (! empty ( $produkte )) { // Sobald nicht alle 3 Anschlüsse ausgewählt sind, ist das Array leer
+						$this->view->vorschläge = $produkte;
+					} else {
+						$this->view->keineVorschläge = true;
+					}
+					
+					$this->view->showVor = true; // Vorschläge werden angezeigt
+					
+					/*
+					 * $tempVal = $form->getElement('tempVal'); $heightVal = $form->getElement('heightVal'); $widthVal = $form->getElement('widthVal'); $lengthVal = $form->getElement('lengthVal');
+					 */
       			
         		/*
       			if(!$anschluss1->isChecked() && !$anschluss2->isChecked() && !$anschluss3->isChecked()){
@@ -131,79 +112,44 @@ class WaermetauscherController extends Zend_Controller_Action
       			}
       			*/
         		}
-        }
-    }
-
-    public function geloetetAction()
-    {
-    	$request = $this->getRequest ();
-		$art = $request->getParam ( 'artikel' );
-		if (null != $art) {
-			//Artikeldaten aus db auslesen und aufbereiten
-			//Statement: SELECT * FROM Waermetauscher WHERE ID like ?, $art;
-			$entries = array();
-			$art_name = "Testartikel";//$entries['Artikelname'];
-			$art_temp = 1200; //$entries['Temperatur'];
-			$art_loc  = "Draussen";//$entries['Einsatzgebiet'];
-			// Anschlüsse fehlen noch
-			$art_height = 1230; //$entries['Maximale Höhe'];
-			$art_width  = 123; // $entries['Maximale Breite'];
-			
-			$container = array($art_name,$art_loc,$art_temp,$art_height,$art_width,$art);
-			
-			$this->view->art_display = $container;
-			
-					} else {
-			$this->view->message = 'Artikel konnte nicht gefunden werden';
-			if (isset ( $_SERVER ['HTTP_REFERER'] )) {
-				$this->view->link = $_SERVER ['HTTP_REFERER'];
-			} else {
-				$this->view->link = '/Waermetauscher';
 			}
 		}
-    }
-
-    public function geschraubtAction()
-    {
-		//befindet sich alles im view da statische seite 
-    }
-
-    public function rohrbuendelAction()
-    {
-    	//befindet sich alles im view da statische seite
-    }
-
-    public function bearbeitenAction()
-    {
-		// action body
-    }
-
-    public function hinzufuegenAction()
-    {
-        $form = new Application_Form_WtErstellen();
-        
-        $this->view->form = $form;
-    }
-
-    public function entfernenAction()
-    {	/*
-        Artikeldaten aus Registry/Session laden
-        $artID = $_SESSION['artikel'];
-        $artID = $artID['ID'];
-        if(buttonpushed){
-        Query: 
-        DELETE FROM artikel WHERE artikelID IN(SELECT artikelID	FROM artikel
-		WHERE artikelID = '$artID' );
+		public function geloetetAction() {
+			$request = $this->getRequest ();
+			$art = $request->getParam ( 'artikel' );
+			//TODO Ausnahmen abfangen! ->schlumpfhandling -.-
+			if (true) {
+				$db_mapper = new Application_Model_WaermetauscherMapper ();
+				$data_object = $db_mapper->getWaermetauscherByModel ( $art );
+				$this->view->dbdata = $data_object;
+			} else {
+				$this->view->message = 'Artikel konnte nicht gefunden werden';
+				if (isset ( $_SERVER ['HTTP_REFERER'] )) {
+					$this->view->link = $_SERVER ['HTTP_REFERER'];
+				} else {
+					$this->view->link = '/Waermetauscher';
+				}
+			}
 		}
-		
-		$this->view->artikel = $_SESSION['artikel'];
-		
-		*/
-    	
-    }
-
-
-}
+		public function geschraubtAction() {
+			// befindet sich alles im view da statische seite
+		}
+		public function rohrbuendelAction() {
+			// befindet sich alles im view da statische seite
+		}
+		public function bearbeitenAction() {
+			// action body
+		}
+		public function hinzufuegenAction() {
+			$form = new Application_Form_WtErstellen ();
+			
+			$this->view->form = $form;
+		}
+		public function entfernenAction() { /*
+		   * Artikeldaten aus Registry/Session laden $artID = $_SESSION['artikel']; $artID = $artID['ID']; if(buttonpushed){ Query: DELETE FROM artikel WHERE artikelID IN(SELECT artikelID	FROM artikel WHERE artikelID = '$artID' ); } $this->view->artikel = $_SESSION['artikel'];
+		   */
+		}
+	}
 
 
 
