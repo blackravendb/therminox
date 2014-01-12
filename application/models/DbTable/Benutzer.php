2bd5884062df4e53a1a725f1aea6f75b7346e8fe
blackravendb <dennis.brandmueller@stud.fh-rosenheim.de>
2014-01-12 22:15:43 +0100

    transaktionen in benutzer, bugfix für benutzeranrede 	modified:   application/models/DbTable/Benutzer.php

--- application/models/DbTable/Benutzer.php
+++ application/models/DbTable/Benutzer.php
@@ -39,128 +39,168 @@ class Application_Model_DbTable_Benutzer extends Zend_Db_Table_Abstract
     }
     
     public function getBenutzer($email){
-    	$this->select
-    		->where('email = ?', $email);
-    	
-    	 $data = $this->fetchRow($this->select);
-    	 
-    	 //Select Befehl wieder zurücksetzen
-    	 $this->init();
-    	 
-    	 //Dazugehörige Adressen abfragen
-    	 $adressen = $data->findDependentRowset('Application_Model_DbTable_Adresse','benutzer');
-
-    	 //Alle Daten in Array zusammenfassen zum returnen
-    	 return array($data->toArray(), $adressen->toArray());
+    	try {
+	    	$this->select
+	    		->where('email = ?', $email);
+	    	
+	    	 $data = $this->fetchRow($this->select);
+	    	 
+	    	 //Select Befehl wieder zurücksetzen
+	    	 $this->init();
+	    	 
+	    	 //Dazugehörige Adressen abfragen
+	    	 $adressen = $data->findDependentRowset('Application_Model_DbTable_Adresse','benutzer');
+			 
+	    	 if(!empty($adressen)) {
+	    	 	$adressenData = $adressen->toArray();
+	    	 	foreach($adressenData as $value) {
+	    	 		$adressenData['anrede'] = $this->getAnrede_idByAnrede($adressenData['anrede_id']);
+	    	 	}
+	    	 }
+	    	 //Alle Daten in Array zusammenfassen zum returnen
+	    	 return array($data->toArray(), $adressenData);
+    	}
+    	catch(Exception $e) {
+    		return false;
+    	}
     }
     
     public function fetchAll(){
-    	$this->select
-       ->from('benutzer')
-    	->join('anrede', 'benutzer.anrede_id = anrede.id');
-    	
-    	$data = parent::fetchAll($this->select);
-    	
-    	//Select Befehl wieder zurücksetzen
-    	$this->init();
-    	
-    	$ret = array();
-    	
-    	//Dazugehörige Adressen abfragen
-    	foreach($data as $key => $value){
-    		$adressen = $value->findDependentRowset('Application_Model_DbTable_Adresse','benutzer');
-    		
-    		$ret[] = array($value->toArray(), $adressen->toArray());
+    	try {
+	    	$this->select
+	       ->from('benutzer')
+	    	->join('anrede', 'benutzer.anrede_id = anrede.id');
+	    	
+	    	$data = parent::fetchAll($this->select);
+	    	
+	    	//Select Befehl wieder zurücksetzen
+	    	$this->init();
+	    	
+	    	$ret = array();
+	    	
+	    	//Dazugehörige Adressen abfragen
+	    	foreach($data as $key => $value){
+	    		$adressen = $value->findDependentRowset('Application_Model_DbTable_Adresse','benutzer');
+	    		
+	    		$ret[] = array($value->toArray(), $adressen->toArray());
+	    	}
+	    	
+	    	return $ret;
     	}
-    	
-    	return $ret;
+    	catch(Exception $e) {
+    		return false;
+    	}
     }
     
     public function updateBenutzer(Application_Model_Benutzer $benutzer) {
-    	
-    	$where = $this->getAdapter()->quoteInto('email = ?', $benutzer->getEmail());
-    	
-    	
-    	//Alle Felder, die sich nicht verändert haben, löschen
-    	$benutzerData = $benutzer->toArray();
-    	foreach($benutzerData as $key => $value){
-    		if(!$benutzer->isChanged($key)){
-    			unset($benutzerData[$key]);
-    		}
+    	try {
+    		$this->getAdapter()->beginTransaction();
+	    	$where = $this->getAdapter()->quoteInto('email = ?', $benutzer->getEmail());
+	    	
+	    	
+	    	//Alle Felder, die sich nicht verändert haben, löschen
+	    	$benutzerData = $benutzer->toArray();
+	    	foreach($benutzerData as $key => $value){
+	    		if(!$benutzer->isChanged($key)){
+	    			unset($benutzerData[$key]);
+	    		}
+	    	}
+	    	
+	    	//Fremde Tabellen überrüfen:
+	    	//Überprüfen, ob sich Anrede verändert hat
+	    	if(key_exists("anrede", $benutzerData)) {
+	    		
+	    		$benutzerData['anrede_id'] = $this->getAnrede_idByAnrede($benutzerData['anrede']);
+	    		unset($benutzerData['anrede']);
+	    	}
+	    	
+	    	//Überprüfen, ob sich Rechnungsadressen verändert hat
+	    	if(key_exists('rechnungsadresse', $benutzerData)){
+	    		foreach($benutzerData['rechnungsadresse'] as $value){
+	    			$this->getAdresseDbTable()->changeAdresse($value, $benutzer->getEmail());
+	    		}
+	    		unset($benutzerData['rechnungsadresse']);
+	    	}
+	    	
+	    	//Überprüfen, ob sich Lieferadresse verändert hat
+	    	if(key_exists('lieferadresse', $benutzerData)){
+	    		foreach($benutzerData['lieferadresse'] as $value) {
+	    			$this->getAdresseDbTable()->changeAdresse($value, $benutzer->getEmail());
+	    		}
+	    		unset($benutzerData['lieferadresse']);
+	    	}
+	    	
+	    	//Überprüfen ob Liefer- Rechnungsadressen gelöscht werden müssen
+	    	$adressen2delete = $benutzer->getAdressen2delete();
+	    	if(!empty($adressen2delete))
+	    		$this->getAdresseDbTable()->deleteAdresse($adressen2delete);
+	    	
+	    	//überprüfen, ob sich sonst noch Felder verändert haben
+	    	if(sizeof($benutzerData) == 0){
+	    		$this->getAdapter()->commit();
+	    		return true;
+	    	}
+	    	
+	    	return $this->update($benutzerData, $where);
+	    	$this->getAdapter()->commit();
+	    	return true;
     	}
-    	
-    	//Fremde Tabellen überrüfen:
-    	//Überprüfen, ob sich Anrede verändert hat
-    	if(key_exists("anrede", $benutzerData)) {
-    		
-    		$benutzerData['anrede_id'] = $this->getAnrede_idByAnrede($benutzerData['anrede']);
-    		unset($benutzerData['anrede']);
+    	catch(Exception $e) {
+    		$this->getAdapter()->rollBack();
+    		return false;
     	}
-    	
-    	//Überprüfen, ob sich Rechnungsadressen verändert hat
-    	if(key_exists('rechnungsadresse', $benutzerData)){
-    		foreach($benutzerData['rechnungsadresse'] as $value){
-    			$this->getAdresseDbTable()->changeAdresse($value, $benutzer->getEmail());
-    		}
-    		unset($benutzerData['rechnungsadresse']);
-    	}
-    	
-    	//Überprüfen, ob sich Lieferadresse verändert hat
-    	if(key_exists('lieferadresse', $benutzerData)){
-    		foreach($benutzerData['lieferadresse'] as $value) {
-    			$this->getAdresseDbTable()->changeAdresse($value, $benutzer->getEmail());
-    		}
-    		unset($benutzerData['lieferadresse']);
-    	}
-    	
-    	//Überprüfen ob Liefer- Rechnungsadressen gelöscht werden müssen
-    	$adressen2delete = $benutzer->getAdressen2delete();
-    	if(!empty($adressen2delete))
-    		$this->getAdresseDbTable()->deleteAdresse($adressen2delete);
-    	
-    	//überprüfen, ob sich sonst noch Felder verändert haben
-    	if(sizeof($benutzerData) == 0){
-    		return;
-    	}
-    	
-    	return $this->update($benutzerData, $where);
     }
 
     public function insertBenutzer(Application_Model_Benutzer $benutzer, $email) {
-    	$benutzerData = $benutzer->toArray();
-    	$benutzerData['email'] = $email;
-    	
-    	
-    	//Anrede ID ermitteln
-    	$benutzerData['anrede_id'] = $this->getAnrede_idByAnrede($benutzerData['anrede']);
-    	unset($benutzerData['anrede']);
-
-    	//Liefer und Rechnungsadressen für späteres schreiben zwischenspeichern
-    	$lieferadressen = $benutzer->getLieferadresse();
-    	$rechnungsadresse = $benutzer->getRechnungsadresse();
-    	
-    	unset($benutzerData['lieferadresse']);
-    	unset($benutzerData['rechnungsadresse']);
-    	
-    	$this->insert($benutzerData);
-
-    	//Liefer und Rechnungsadressen schreiben
-    	if(!empty($lieferadressen)){
-    		foreach($lieferadressen as $value){
-    			$this->getAdresseDbTable()->changeAdresse($value, $email);
-    		}
+    	try {
+    		$this->getAdapter()->beginTransaction();
+	    	$benutzerData = $benutzer->toArray();
+	    	$benutzerData['email'] = $email;
+	    	
+	    	
+	    	//Anrede ID ermitteln
+	    	$benutzerData['anrede_id'] = $this->getAnrede_idByAnrede($benutzerData['anrede']);
+	    	unset($benutzerData['anrede']);
+	
+	    	//Liefer und Rechnungsadressen für späteres schreiben zwischenspeichern
+	    	$lieferadressen = $benutzer->getLieferadresse();
+	    	$rechnungsadresse = $benutzer->getRechnungsadresse();
+	    	
+	    	unset($benutzerData['lieferadresse']);
+	    	unset($benutzerData['rechnungsadresse']);
+	    	
+	    	$this->insert($benutzerData);
+	
+	    	//Liefer und Rechnungsadressen schreiben
+	    	if(!empty($lieferadressen)){
+	    		foreach($lieferadressen as $value){
+	    			$this->getAdresseDbTable()->changeAdresse($value, $email);
+	    		}
+	    	}
+	    	if(!empty($rechnungsadresse)){
+	    		foreach($rechnungsadresse as $value){
+	    			$this->getAdresseDbTable()->changeAdresse($value, $email);
+	    		}
+	    	}
+	    	$this->getAdapter()->commit();
+	    	return true;
     	}
-    	if(!empty($rechnungsadresse)){
-    		foreach($rechnungsadresse as $value){
-    			$this->getAdresseDbTable()->changeAdresse($value, $email);
-    		}
+    	catch(Exception $e) {
+    		$this->getAdapter()->rollBack();
+    		return false;
     	}
     }
     
     public function deleteBenutzer ($email){
+    	try {
     	$where = $this->getAdapter()->quoteInto('email = ?', $email);
     	
-    	return $this->delete($where);
+    	$this->delete($where);
+    	return true;
+    	}
+    	catch(Exception $e) {
+    		return false;
+    	}
     }
     
     public function existEmail($email) {
@@ -173,7 +213,7 @@ class Application_Model_DbTable_Benutzer extends Zend_Db_Table_Abstract
     	$data = $this->fetchRow($select);
     	$this->init();
     	
-    	if($data != "") 
+    	if(!empty($data)) 
     		return true;
     	
     	return false;
