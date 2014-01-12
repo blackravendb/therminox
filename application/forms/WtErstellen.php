5f8f327fe6536d790a0e7f0cde1525ad83632d42
SimoKutlin <simo.kutlin@gmail.com>
2014-01-12 19:26:28 +0100

    Wärmetauscher formular update

--- application/forms/WtErstellen.php
+++ application/forms/WtErstellen.php
@@ -12,7 +12,7 @@ class Application_Form_WtErstellen extends App_Form {
 		$lengthVal = new Zend_Validate_Between(array('min' => 20, 'max' => 500));
 
 		$name = new Zend_Form_Element_Text('Model');
-		$name->setLabel('Artikelname:')
+		$name->setLabel('Artikelmodell:')
 		->addFilter('StripTags')
 		->addFilter('StringTrim');
 		 		 		 	
@@ -26,13 +26,17 @@ class Application_Form_WtErstellen extends App_Form {
 		->addFilter('StripTags')
 		->addFilter('StringTrim');
 
-		$einsatzgbt = new Zend_Form_Element_Select('Einsatzgebiet');
-		$einsatzgbt->setLabel('Einsatzgebiet:')
-		->addMultiOption('Fernwärme', 'Fernwärme')
-		->addMultiOption('Solaranlage', 'Solaranlage')
-		->addMultiOption('Erdbohrung', 'Erdbohrung')
+		$einsatzgbt = new Zend_Form_Element_MultiCheckbox('Einsatzgebiet');
+		$einsatzgbt->setLabel('Einsatzgebiete:')
 		->addFilter('StripTags')
 		->addFilter('StringTrim');
+		$db_mapper = new Application_Model_WaermetauscherMapper();
+		$gebiete = $db_mapper->getEinsatzgebietListe();
+		foreach ($gebiete as $gebiet){
+		$einsatzgbt->addMultiOption("$gebiet", "$gebiet");
+		}
+		
+		
 		 
 		$anschluss = new Zend_Form_Element_MultiCheckbox('Anschluss', array(
 				'multiOptions' => array(
@@ -43,52 +47,30 @@ class Application_Form_WtErstellen extends App_Form {
 		));
 		$anschluss->setLabel('Anschlüsse:');
 		
-		$material = new Zend_Form_Element_Text('Stutzenmaterial');
+		$material = new Zend_Form_Element_Select('Stutzenmaterial');
 		$material->setLabel('Stutzenmaterial:')
 		->addFilter('StripTags')
 		->addFilter('StringTrim');
-	/*
-		$pictures = new Zend_Form_Element_File('Bilder');
-		$pictures->setLabel('Artikelbilder hochladen')
-		->addValidator('Extension', false, 'png')
-		->setDestination('_files/images/waermetauscher')
-		->addValidator('Size', false, 102400) // Limitiert auf 100k
-		->setMaxFileSize(102400); // Limitiert dateigröße clientseitig
-		*/
-		/*
-		 $anschluss = new Zend_Form_Element_Multiselect('Anschluss');
-		$anschluss->setLabel('Anschlüsse:')
-		//->addMultiOptions($anschlüsse); warten auf Datenbank
-		->addMultiOption('3/8" IG','3/8" IG')
-		->addMultiOption('1/2" AG', '1/2" AG')
-		->addMultiOption('3/4" AG', '3/4" AG');
-		*/
-
-		/*	$maxHeight = new ZendX_JQuery_Form_Element_Slider('Hoehe');
-		 $maxHeight->setLabel('Maximale Höhe:')
-		->setJQueryParams(array('min' => 170, 'max' => 1100, 'value' => 1));
-
-		$maxWidth = new ZendX_JQuery_Form_Element_Slider('Breite');
-		$maxWidth->setLabel('Maximale Breite:')
-		->setJQueryParams(array('min' => 70, 'max' => 400, 'value' => 1));
-
-		$maxLength = new ZendX_JQuery_Form_Element_Slider('Länge');
-		$maxLength->setLabel('Maximale Länge:')
-		->setJQueryParams(array('min' => 20, 'max' => 500, 'value' => 1)); */
-
+		$materialien = $db_mapper->getStutzenmaterialListe();
+		foreach ($materialien as $einmaterial){
+			$material->addMultiOption("$einmaterial", $einmaterial);
+		}
+		
+		
+	
 		$maxHeight = new Zend_Form_Element_Text('Hoehe');
-		$maxHeight->setLabel('Maximale Höhe in mm:')
+		$maxHeight->setLabel('Maximale Höhe in cm:')
 		->addFilter('StripTags')
 		->addFilter('StringTrim');
 
 		$maxWidth = new Zend_Form_Element_Text('Breite');
-		$maxWidth->setLabel('Maximale Breite in mm:')
+		$maxWidth->setLabel('Maximale Breite in cm:')
 		->addFilter('StripTags')
 		->addFilter('StringTrim');
 
 		$submit = new Zend_Form_Element_Submit('submit');
 		$submit->setLabel('Artikel erzeugen');
 		 
-		$this->addElements(array($name,$temp, $pressure, $einsatzgbt, $anschluss, $maxHeight, $maxWidth, $material,$pictures, $submit));
+		$this->addElements(array($name,$temp, $pressure, $einsatzgbt, $anschluss, $maxHeight, $maxWidth, $material, $submit));
 	}
 }
