b8ad91dbca6950f4cc27efc7d21910de8ec535bd
SimoKutlin <simo.kutlin@gmail.com>
2014-01-12 16:37:05 +0100

    Angebote Bearbeiten

---
+++ application/forms/AngebotBearbeiten.php
@@ -0,0 +1,27 @@
+<?php
+class Application_Form_AngebotBearbeiten extends App_Form {
+	public function __construct($options = null) {
+		parent::__construct ( $options );
+		
+		$userdata = $this->_userinfo;
+		$articledata = $this->_articleinfo;
+		
+		$extraInfo = new Zend_Form_Element_Textarea ( 'extraInfo' );
+		$extraInfo->setLabel ( 'Weiter würden mich folgende Informationen interessieren:' )->addFilter ( 'StripTags' )->addFilter ( 'StringTrim' );
+		
+		$addMore = new Zend_Form_Element_Submit ( 'addMore' );
+		$addMore->setLabel ( 'Weitere Artikel zum Angebot hinzufügen' );
+		
+		$submit = new Zend_Form_Element_Submit ( 'submit' );
+		$submit->setLabel ( 'Angebot absenden' );
+		
+		$this->addElements ( array (
+				$extraInfo,
+				$addMore,
+				$submit 
+		) );
+		$this->addElement ( 'hidden', 'return', array (
+				'value' => Zend_Controller_Front::getInstance ()->getRequest ()->getRequestUri () 
+		) );
+	}
+}
